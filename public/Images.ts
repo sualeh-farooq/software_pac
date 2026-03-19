5db96cfb6b661fff52276cacdec7cbef3324d3e2
sualeh-farooq <sualehfarooq65@gmail.com>
2026-03-20 02:56:36 +0500

    Update Images.ts

--- public/Images.ts
+++ public/Images.ts
@@ -1,14 +1,9 @@
 import ICFAQStars from "./assets/ic-faq-stars.png";
 import PhoneIcon from "./assets/phone.png";
 import EmailIcon from "./assets/email.png";
-import BlackFundingLogo from "./assets/black_flag.png"
 import Facebook from "./assets/facebook-footer.png";
-import ModalBackground from "./assets/background-modal.jpg";
-
 
 export const Images = {
-	BlackFundingLogo,
-	ModalBackground,
 	Facebook,
 	ICFAQStars,
 	PhoneIcon,
